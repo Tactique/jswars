5aeea08c0657f02ad4b817f21e971a65f67e499d
Eric Brown <eric.d.brown23@gmail.com>
2013-11-06 21:56:08 -0500

    Encapsulate the canvas context and update on changing window size

--- src/js/App.js
+++ src/js/App.js
@@ -1,12 +1,31 @@
+var gfx = {
+    ctx: null,
+    width: 0,
+    height: 0
+};
+
 $(document).ready(function() {
     initCanvas($(window).width(), $(window).height());
 });
 
+$(window).resize(function() {
+    resizeCanvas($(window).width(), $(window).height());
+});
+
 var initCanvas = function(width, height) {
-    // Create the canvas
     var canvas = document.createElement("canvas");
-    var ctx = canvas.getContext("2d");
+    canvas.id = "canvas";
+    document.getElementById("canvas_land").appendChild(canvas);
+    resizeCanvas(width, height);
+
+    gfx.ctx = canvas.getContext("2d");
+}
+
+var resizeCanvas = function(width, height) {
+    var canvas = document.getElementById("canvas");
     canvas.width = width;
     canvas.height = height;
-    document.getElementById("canvas_land").appendChild(canvas);
+
+    gfx.width = width;
+    gfx.height = height;
 }
