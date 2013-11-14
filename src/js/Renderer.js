8dbb22a3a597ae1f8d01d5549208c842876c5c1f
Eric Brown <eric.d.brown23@gmail.com>
2013-11-13 22:22:44 -0500

    Beginning world drawing functions

--- src/js/Renderer.js
+++ src/js/Renderer.js
@@ -4,3 +4,21 @@ var drawLine = function(sx, sy, dx, dy) {
     gfx.ctx.lineTo(dx, dy);
     gfx.ctx.stroke();
 }
+
+var drawWorld = function(world) {
+
+    drawGrid(world);
+}
+
+var drawGrid = function(world) {
+
+    for (var x = 0; x <= world.getWidth(); x++) {
+        var cell_x = (camera.getWidth() / world.getWidth()) * x;
+        drawLine(cell_x, 0, cell_x, camera.getHeight());
+    }
+
+    for (var y = 0; y <= world.getHeight(); y++) {
+        var cell_y = (camera.getHeight() / world.getHeight()) * y;
+        drawLine(0, cell_y, camera.getWidth(), cell_y);
+    }
+}
