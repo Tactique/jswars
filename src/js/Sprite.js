b11834c74cc4dfd3a4662fdc096b3c43d0973d81
Eric Brown <eric.d.brown23@gmail.com>
2013-11-20 22:55:18 -0500

    Add sprite manager to track all sprites in one place

--- src/js/Sprite.js
+++ src/js/Sprite.js
@@ -1,3 +1,27 @@
+function SpriteManager() {
+    function addSprite(spriteName, url, srcPos, width, height, animRate, animSeq) {
+        sprites[spriteName] = new Sprite(url, srcPos, width, height, animRate, animSeq);
+    }
+
+    function update(dt) {
+        for (var key in sprites) {
+            if (sprites.hasOwnProperty(key)) {
+                sprites[key].update(dt);
+            }
+        }
+    }
+
+    sprites = {};
+
+    this.addSprite = function(spriteName, url, srcPos, width, height, animRate, animSeq) {
+        return addSprite(spriteName, url, srcPos, width, height, animRate, animSeq);
+    }
+
+    this.update = function(dt) {
+        update(dt);
+    }
+}
+
 function Sprite(url, srcPos, width, height, animRate, animSeq) {
     this.url = url;
     this.srcPos = srcPos;
