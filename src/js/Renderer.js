f95507adc2f1ed0ac08e05e52e1cd604156a4a68
Eric Brown <eric.d.brown23@gmail.com>
2013-11-26 20:38:41 -0500

    Render sprites!

--- src/js/Renderer.js
+++ src/js/Renderer.js
@@ -15,6 +15,22 @@ function drawWorld() {
     drawGrid(game.world);
 }
 
+function drawSprite(x, y, sprite) {
+    var img = assets.get(sprite.url);
+    var pos = sprite.getFramePosition();
+
+    var cam_pos = camera.transformToCameraSpace(x, y);
+    // this is incorrect, but is ok for now since all sprites so far will
+    // only be 1 cell in size
+    var cam_size = camera.multZoomFactor(1, 1);
+
+    gfx.ctx.drawImage(img,
+                      pos['x'] * sprite.width, pos['y'] * sprite.height,
+                      sprite.width, sprite.height,
+                      cam_pos.cam_x, cam_pos.cam_y,
+                      cam_size.cam_w, cam_size.cam_h);
+}
+
 function drawGrid(world) {
     gfx.ctx.strokeStyle = "#000000";
     for (var x = 0; x <= world.getWidth(); x++) {
