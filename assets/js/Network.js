dcae2abcbfe7c5bc34f3cc546ae601e88db1fd89
Eric Brown <eric.d.brown23@gmail.com>
2013-12-03 22:12:30 -0500

    Add dummy methods to prove functionality of proxy

--- assets/js/Network.js
+++ assets/js/Network.js
@@ -1,4 +1,19 @@
 host = window.location.host.split(":")[0];
 port = "8888"
 ws_addr = "ws://" + host + ":" + port + "/ws"
-// conn = new WebSocket(ws_addr);
+conn = new WebSocket(ws_addr);
+
+// When the connection is open, send some data to the server
+conn.onopen = function () {
+  conn.send('Ping'); // Send the message 'Ping' to the server
+};
+
+// Log errors
+conn.onerror = function (error) {
+  console.log('WebSocket Error ' + error);
+};
+
+// Log messages from the server
+conn.onmessage = function (e) {
+  console.log('Server: ' + e.data);
+};
