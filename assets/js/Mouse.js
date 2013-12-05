c5e5d898fe571be4273eb58214298ce5430e2c69
Eric Brown <eric.d.brown23@gmail.com>
2013-12-04 20:01:57 -0500

    Hackily fix the initial mouse position

--- assets/js/Mouse.js
+++ assets/js/Mouse.js
@@ -3,16 +3,25 @@ buttonCharToCode = {"Left": 1, "Middle": 2, "Right": 3};
 
 function Mouse() {
     // It'd be good to get this info on construction
-    this.x = 0;
-    this.y = 0;
+    this.x = -1;
+    this.y = -1;
     this.dx = 0;
     this.dy = 0;
 
     this.UpdatePosition = function(nx, ny) {
-        this.dx = nx - this.x;
-        this.dy = ny - this.y;
-        this.x = nx;
-        this.y = ny;
+        // It's impossible to know where the mouse is when it's first created,
+        // before a mousemove event is fired. If we do nothing the initial
+        // dx and dy are incorrect, so we hack this to not update the dx and dy
+        // till we've gotten one mousemove
+        if (this.x == -1 && this.y == -1) {
+            this.x = nx;
+            this.y = ny;
+        } else {
+            this.dx = nx - this.x;
+            this.dy = ny - this.y;
+            this.x = nx;
+            this.y = ny;
+        }
     }
 
     this.ButtonDown = function(button) {
