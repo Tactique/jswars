8fbb730e54aca4be28cdf37f5b51cfc0860c1a81
Eric Brown <eric.d.brown23@gmail.com>
2013-12-17 23:09:03 -0500

    Send world view request and parse the response

--- assets/js/Network.js
+++ assets/js/Network.js
@@ -1,4 +1,5 @@
 function Network() {
+    // To server functions and constants
     function sendGameRequest(numPlayers) {
         var message = {"NumPlayers": numPlayers}
         sendMessage(NEW_GAME_CMD, message)
@@ -9,8 +10,13 @@ function Network() {
         sendMessage(CLIENT_INFO_CMD, message);
     }
 
+    function sendViewWorld() {
+        sendMessage(VIEW_WORLD_CMD, {});
+    }
+
     CLIENT_INFO_CMD = "clientInfo";
     NEW_GAME_CMD = "newGame";
+    VIEW_WORLD_CMD = "view";
 
     host = window.location.host.split(":")[0];
     port = "8888"
@@ -25,6 +31,15 @@ function Network() {
         conn.send(packet)
     }
 
+    // From server functions and constants
+    this.packetHandlers = {
+        "view": parseViewWorld,
+    }
+
+    function parseViewWorld(status, world) {
+        console.log(world);
+    }
+
     // When the connection is open, send some data to the server
     conn.onopen = function () {
         network.sendClientInfo(getPlayerId());
@@ -37,7 +52,21 @@ function Network() {
 
     // Log messages from the server
     conn.onmessage = function (e) {
-        console.log('Server: ' + e.data);
+        var cmds = e.data.actualSplit(DELIM, 3);
+        console.log(cmds);
+        var pkt_type = cmds[0];
+        var status = cmds[1];
+        if (cmds.length > 2) {
+            console.log(cmds[2]);
+            var dataObj = JSON.parse(cmds[2]);
+            console.log(pkt_type, status, dataObj);
+            var handler = this.packetHandlers[pkt_type];
+            if (handler != null) {
+                handler(status, dataObj);
+            } else {
+                console.log("Recieved unknown command:", pkt_type);
+            }
+        }
     };
 
     this.sendGameRequest = function(numPlayers) {
@@ -47,4 +76,13 @@ function Network() {
     this.sendClientInfo = function(playerId) {
         sendClientInfo(playerId);
     }
+
+    this.sendViewWorld = function() {
+        sendViewWorld();
+    }
+}
+
+String.prototype.actualSplit = function(sep, maxsplit) {
+    var split = this.split(sep);
+    return maxsplit ? split.slice(0, maxsplit - 1).concat(split.slice(maxsplit - 1, split.length).join(":")) : split;
 }
