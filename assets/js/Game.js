99ea37a69ce92a27c055cf490fc3e18772cc6e74
Eric Brown <eric.d.brown23@gmail.com>
2014-02-05 17:59:15 -0500

    Move requestAnimFrame to its appropriate file

--- assets/js/Game.js
+++ assets/js/Game.js
@@ -1,17 +1,3 @@
-// A cross-browser requestAnimationFrame
-// See https://hacks.mozilla.org/2011/08/animating-with-javascript-from-setinterval-to-requestanimationframe/
-var requestAnimFrame = (function(){
-    return window.requestAnimationFrame    ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame    ||
-        window.oRequestAnimationFrame      ||
-        window.msRequestAnimationFrame     ||
-        function(callback){
-            window.setTimeout(callback, 1000 / 60);
-        };
-})();
-
-
 function Game() {
     function init() {
         this.Inputs = new InputManager();
--- assets/js/Renderer.js
+++ assets/js/Renderer.js
@@ -4,6 +4,19 @@ var renderers = {
     "MENU_CONTROL" : drawMenu
 }
 
+// A cross-browser requestAnimationFrame
+// See https://hacks.mozilla.org/2011/08/animating-with-javascript-from-setinterval-to-requestanimationframe/
+var requestAnimFrame = (function(){
+    return window.requestAnimationFrame    ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        function(callback){
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
 var specialRenderer;
 function SpecialRenderer() {
     function addLayer(key, priority, renderfunc, renderable) {
