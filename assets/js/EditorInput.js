7c458310bc6fe7c7d14ede0fa03816473a0f1899
Eric Brown <eric.d.brown23@gmail.com>
2014-02-15 13:00:48 -0500

    Fill out stub rendering methods for editor

---
+++ assets/js/EditorInput.js
@@ -0,0 +1,29 @@
+function handleCameraKeyboard(keyboard) {
+    var camMove = {'x': 0, 'y': 0};
+    if (keyboard.KeyDown("Left")) {
+        camMove['x'] -= 5;
+    }
+    if (keyboard.KeyDown("Right")) {
+        camMove['x'] += 5;
+    }
+    if (keyboard.KeyDown("Up")) {
+        camMove['y'] -= 5;
+    }
+    if (keyboard.KeyDown("Down")) {
+        camMove['y'] += 5;
+    }
+    camera.processMove(camMove);
+}
+
+function handleCameraMouse(mouse) {
+    if (mouse.currentState == mouseStates.LeftDrag) {
+        var camMove = {'x': mouse.dx, 'y': mouse.dy};
+        mouse.dx = 0;
+        mouse.dy = 0;
+        camera.processMove(camMove);
+    } else if(mouse.lastState == mouseStates.LeftDown &&
+              mouse.currentState == mouseStates.LeftUp) {
+
+    }
+}
+
