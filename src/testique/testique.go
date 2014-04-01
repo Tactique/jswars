765d75ef125ac4a78a2b6919ff65cea084394637
Eric Brown <eric.d.brown23@gmail.com>
2014-04-01 09:32:09 -0400

    Initial commit of test server

---
+++ src/testique/testique.go
@@ -0,0 +1,83 @@
+package testique
+
+import (
+    "net"
+    "flag"
+    "strings"
+    "warserver/logger"
+    "warserver/PortMgmt"
+)
+
+const (
+    RECV_BUF_LEN = 1024
+)
+
+var serverPort PortMgmt.PortInfo
+
+var testHandlers = make(map [string]func(msg string) string)
+
+func Main() {
+    portString := flag.String("port", ":5269", "Test server port")
+    flag.Parse()
+
+    serverPort = PortMgmt.NewPortInfo(*portString)
+
+    // might as well go straight to std out for a testserver
+    logger.SetupLoggerHelper("/dev/stdout")
+
+    logger.Infof("Accepting testserver connections on port %s", portString)
+    handleConnections(serverPort.Port)
+}
+
+func setupHandlers() {
+    testHandlers["newGame"] = testNewGame
+}
+
+func testNewGame(msg string) string {
+    return "totes new, bro"
+}
+
+func handleConnections(port string) {
+    ln, err := net.Listen("tcp", port)
+    if err != nil {
+        logger.Fatalf("Could not open socket for listening: %s", err)
+    }
+    for {
+        conn, err := ln.Accetp()
+        if err != nil {
+            logger.Errorf("Could not accept connection from client: %s", err)
+            continue
+        }
+        sconn := socketConn{sock: conn}
+    }
+}
+
+func serveConnection(sock *socketConn) {
+    for {
+        msg, err := sock.Read()
+        if err != nil {
+            // Should gracefully shutdown someday on EOF or UnexpectedEOF
+            logger.Errorf("Error reading from client: %s", err)
+            break
+        }
+        resp := parseMessage(string(msg))
+        err = sock.Write([]byte(resp))
+        if err != nil {
+            logger.Errorf("Error writing to client: %s", err)
+            break
+        }
+    }
+}
+
+func parseMessage(msg string) string {
+    cmds := strings.SplitN(msg, ":", 2)
+    if len(cmds) == 2 {
+        if fun, ok := testHandlers[cmds[0]]; ok {
+            return fun(cmds[1])
+        } else {
+            logger.Warnf("Unrecognized command: %s", cmds[0])
+            return "unrecognized:"
+        }
+    }
+    return "malformed:"
+}
---
+++ testserver.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+    "testique"
+)
+
+func main() {
+    testique.main()
+}
