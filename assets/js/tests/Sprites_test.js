57130eab7aa29e783de1b6161105c2f27d1844bf
Eric Brown <eric.d.brown23@gmail.com>
2014-04-21 23:57:22 -0400

    Update failing test after forgetting I renamed a variable

--- assets/js/tests/Sprites_test.js
+++ assets/js/tests/Sprites_test.js
@@ -18,7 +18,7 @@ test("SpriteManager test", function() {
                             animations, "test", true);
     var sprite = spriteManager.getSprite("test");
     equal(sprite.url, "test.png");
-    deepEqual(sprite.srcPos, {x: 0, y: 0});
+    deepEqual(sprite.sheetPos, {x: 0, y: 0});
     deepEqual(sprite.getFramePosition(), {x: 0, y: 0});
     spriteManager.update(10);
     deepEqual(sprite.getFramePosition(), {x: 1, y: 0});
@@ -38,7 +38,7 @@ test("SpriteManager test", function() {
     // with animate on the source sprite disabled, the updates which affect the
     // cloned sprite should not affect the source sprite
     sprite.animate = false;
-    deepEqual(cloneSprite.srcPos, {x: 0, y: 0});
+    deepEqual(cloneSprite.sheetPos, {x: 0, y: 0});
     deepEqual(cloneSprite.getFramePosition(), {x: 0, y: 0});
     spriteManager.update(10);
     deepEqual(cloneSprite.getFramePosition(), {x: 1, y: 0});
@@ -55,7 +55,7 @@ test("SpriteManager test", function() {
 
     // re-enabling animations for both sprites should cause them both to update
     sprite.animate = true;
-    deepEqual(cloneSprite.srcPos, {x: 0, y: 0});
+    deepEqual(cloneSprite.sheetPos, {x: 0, y: 0});
     deepEqual(cloneSprite.getFramePosition(), {x: 0, y: 0});
     spriteManager.update(10);
     deepEqual(cloneSprite.getFramePosition(), {x: 1, y: 0});
