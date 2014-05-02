0e2a2f996aa7557ba859ddb68f332ae8e4637d21
Quinten Palmer <quintenpalmer@gmail.com>
2014-05-02 00:07:23 -0400

    cType -> cellType rename in database

--- js/AjaxNetwork.js
+++ js/AjaxNetwork.js
@@ -27,10 +27,10 @@ function AjaxNetwork() {
         terrainTable = new Array(response.responseJSON.length);
         for (var i = response.responseJSON.length - 1; i >= 0; i--) {
             var cell = response.responseJSON[i].fields;
-            terrainTable[cell.cType] = function(x, y) {
-                return new Cell(x, y, cell.spriteName, cell.cType);
+            terrainTable[cell.cellType] = function(x, y) {
+                return new Cell(x, y, cell.spriteName, cell.cellType);
             };
-            terrainTypes[cell.spriteName] = cell.cType;
+            terrainTypes[cell.spriteName] = cell.cellType;
         }
     }
 
